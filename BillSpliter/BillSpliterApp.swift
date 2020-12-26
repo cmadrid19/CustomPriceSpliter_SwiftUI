6b90803aafe0be5013428b8a05c5047a9700ea5c
Max <maxim.macari.dam@gmail.com>
2020-12-27 00:47:42 +0100

    Initial Commit

---
+++ BillSpliter/BillSpliterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSpliterApp.swift
+//  BillSpliter
+//
+//  Created by Maxim Macari on 27/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BillSpliterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
